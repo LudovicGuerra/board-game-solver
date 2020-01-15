cf0054803d9314de6461b5f21f27caf175ea6860
LudovicGuerra <ludovic791@gmail.com>
2020-01-15 11:11:45 +0100

    Modifying the venv

--- les-batisseurs/venv/Scripts/easy_install-script.py
+++ les-batisseurs/venv/Scripts/easy_install-script.py
@@ -1,4 +1,4 @@
-#!C:\Users\hermi\Desktop\Ludovic\Git\board-game-solver\les-batisseurs\venv\Scripts\python.exe
+#!F:\Perso\Python\board-game-solver\les-batisseurs\venv\Scripts\python.exe
 # EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==40.8.0','console_scripts','easy_install'
 __requires__ = 'setuptools==40.8.0'
 import re
--- les-batisseurs/venv/Scripts/pip-script.py
+++ les-batisseurs/venv/Scripts/pip-script.py
@@ -1,4 +1,4 @@
-#!C:\Users\hermi\Desktop\Ludovic\Git\board-game-solver\les-batisseurs\venv\Scripts\python.exe
+#!F:\Perso\Python\board-game-solver\les-batisseurs\venv\Scripts\python.exe
 # EASY-INSTALL-ENTRY-SCRIPT: 'pip==19.0.3','console_scripts','pip'
 __requires__ = 'pip==19.0.3'
 import re
